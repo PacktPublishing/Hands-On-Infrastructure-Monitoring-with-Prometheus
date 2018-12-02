81df5b5f2b1d8ec77002ed133d47987e8616f09b
Pedro Araujo <phcrva@gmail.com>
2018-12-02 17:42:47 +0000

    Move vagrant defaults to utils

---
+++ utils/vagrant_defaults.rb
@@ -0,0 +1,9 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+GUI     = false
+RAM     = 256
+DOMAIN  = ".prom.inet"
+NETWORK = "192.168.77."
+NETMASK = "255.255.255.0"
+BOX     = 'bento/ubuntu-18.04'
