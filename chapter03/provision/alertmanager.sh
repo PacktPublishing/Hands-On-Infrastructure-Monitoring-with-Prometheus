3e938127e42cd3a8cfc7b0ffc1273e08552e6efe
Joel Bastos <kintoandar@gmail.com>
2018-12-05 19:37:43 +0000

    Add alertmanager provisioning

---
+++ chapter03/provision/alertmanager.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -e
+
+source /vagrant/utils/defaults.sh
+source /vagrant/utils/helpers.sh
+
+check_requirements curl tar
+
+ARCHIVE="alertmanager-${ALERTMANAGER_VERSION}.linux-amd64.tar.gz"
+
+if ! check_cache "${ARCHIVE}"; then
+  get_archive "https://github.com/prometheus/alertmanager/releases/download/v${ALERTMANAGER_VERSION}/${ARCHIVE}"
+fi
+
+if ! id alertmanager > /dev/null 2>&1 ; then
+  useradd --system alertmanager
+fi
+
+TMPD=$(mktemp -d)
+tar zxf "${CACHE_PATH}/${ARCHIVE}" -C $TMPD --strip-components=1
+
+install -m 0755 $TMPD/alertmanager $TMPD/amtool /usr/sbin/
+install -d -o alertmanager -g alertmanager /var/lib/alertmanager
+install -m 0644 /vagrant/chapter03/configs/alertmanager/alertmanager.service /etc/systemd/system/
+install -m 0644 -D /vagrant/chapter03/configs/alertmanager/alertmanager.yml /etc/alertmanager/alertmanager.yml
+install -m 0644 /vagrant/chapter03/configs/hosts /etc/
+
+systemctl daemon-reload
+systemctl enable alertmanager
+systemctl start alertmanager
