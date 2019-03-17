753cfcb55540f069b9e2ca78d312fa8de162f2b5
Joel Bastos <kintoandar@gmail.com>
2019-03-17 16:10:46 +0000

    Add chapter10 test environments

---
+++ chapter10/provision/grafana.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+
+source /vagrant/utils/defaults.sh
+source /vagrant/utils/helpers.sh
+
+check_requirements curl rsync
+
+ARCHIVE="grafana_${GRAFANA_VERSION}_amd64.deb"
+
+if ! check_cache_deb "${ARCHIVE}"; then
+  get_archive "https://dl.grafana.com/oss/release/${ARCHIVE}"
+fi
+
+DEBIAN_FRONTEND=noninteractive apt-get install -y libfontconfig
+dpkg -i "${CACHE_PATH}/${ARCHIVE}"
+
+rsync -ru /vagrant/chapter10/configs/grafana/{dashboards,provisioning} /etc/grafana/
+
+systemctl daemon-reload
+systemctl enable grafana-server
+systemctl start grafana-server
---
+++ chapter10/provision/hosts.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+NETWORK="$1"
+DOMAIN="$2"
+START_OCTET="$3"
+
+cat <<EOF >/etc/hosts
+127.0.0.1       localhost
+${NETWORK}${START_OCTET}   prometheus${DOMAIN}    prometheus
+${NETWORK}$((START_OCTET+1))   grafana${DOMAIN}       grafana
+
+# The following lines are desirable for IPv6 capable hosts
+::1     localhost ip6-localhost ip6-loopback
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+EOF
