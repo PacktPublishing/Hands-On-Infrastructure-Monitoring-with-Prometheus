a96bbd96d294dd4526d0de2b82b9b29d8c8e7866
Joel Bastos <kintoandar@gmail.com>
2019-05-11 16:43:08 +0100

    Upgrade golang
    This was required due to https://github.com/prometheus/prometheus/pull/5471

--- chapter08/provision/cmd.sh
+++ chapter08/provision/cmd.sh
@@ -6,9 +6,19 @@ source /vagrant/utils/helpers.sh
 
 check_requirements git
 
+ARCHIVE="go${GO_VERSION}.linux-amd64.tar.gz"
+
+apt-get update
+DEBIAN_FRONTEND=noninteractive apt-get install -y graphviz
+
 if ! check_cache_bin "tsdb" || ! check_cache_bin "pprof" ; then
-  apt-get update
-  DEBIAN_FRONTEND=noninteractive apt-get install -y golang-go graphviz
+  if ! check_cache "${ARCHIVE}" ; then
+    get_archive "https://dl.google.com/go/${ARCHIVE}"
+  fi
+
+  tar zxf "${CACHE_PATH}"/"${ARCHIVE}" -C /usr/lib/
+
+  ln -s /usr/lib/go/bin/go /usr/bin/ || true
 
   go get github.com/prometheus/tsdb/cmd/tsdb
   install -m 0755 /root/go/bin/tsdb /vagrant/cache/
--- chapter12/provision/custom-sd.sh
+++ chapter12/provision/custom-sd.sh
@@ -6,9 +6,16 @@ source /vagrant/utils/helpers.sh
 
 check_requirements git
 
+ARCHIVE="go${GO_VERSION}.linux-amd64.tar.gz"
+
 if ! check_cache_bin "custom-sd" ; then
-  apt-get update
-  DEBIAN_FRONTEND=noninteractive apt-get install -y golang-go
+  if ! check_cache "${ARCHIVE}" ; then
+    get_archive "https://dl.google.com/go/${ARCHIVE}"
+  fi
+
+  tar zxf "${CACHE_PATH}"/"${ARCHIVE}" -C /usr/lib/
+
+  ln -s /usr/lib/go/bin/go /usr/bin/ || true
 
   echo "Building custom-sd, this might take a while..."
   go get github.com/prometheus/prometheus/documentation/examples/custom-sd/adapter-usage
